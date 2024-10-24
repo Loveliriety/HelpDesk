e2a9f077a643cb0c8061d05b95fb9f2750b26361
Lloydie17 <johnlloydmiral17@gmail.com>
2024-10-24 19:12:15 +0800

    Added static frontend(login, dashboard, ticket)

---
+++ ASI.Basecode.WebApp/Controllers/TicketController.cs
@@ -0,0 +1,24 @@
+﻿using ASI.Basecode.WebApp.Mvc;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class TicketController : ControllerBase<TicketController>
+    {
+        public TicketController(IHttpContextAccessor httpContextAccessor, 
+                                ILoggerFactory loggerFactory, 
+                                IConfiguration configuration, 
+                                IMapper mapper = null) : base(httpContextAccessor, loggerFactory, configuration, mapper)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
